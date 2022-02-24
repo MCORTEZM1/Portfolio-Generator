ac1afecce64fef559ca638bb56d323bbd3788830
Michael Cortez-Mejia <you@example.com>
2022-02-23 16:25:14 -0800

    added inquirer for command line prompt questions

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const generatePage = require('./src/page-template');
+// const pageHTML = generatePage(name, github);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// // SYNTAX: fs.writeFile(file created, data being written, error callback)
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err; 
+//     // throw ends function and passes to the next catch. If no catch, then program terminates
 
-const [name, github] = profileDataArgs;
-
- // SYNTAX: fs.writeFile(file created, data being written, error callback)
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err; // throw ends function and passes to the next catch. If no catch, then program terminates
-
-    console.log('Portfolio complete! Check out index.html to see the output!')
-})
-
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
 
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-
-
-// below is an example of functionality for reference.
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
